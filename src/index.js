689952081db6dc6d5a4ec882fed5eafbf76b8da9
vodat97 <vothanhdat112@gmail.com>
2022-05-03 21:45:19 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const hbs  = require('express-handlebars');
+//-------------------------------//
+//HTTP logger
+app.use(morgan('combined'));
+//Template engine
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('home');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
